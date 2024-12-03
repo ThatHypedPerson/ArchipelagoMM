fae93dd99ed771e56b16a354c4e8c7dd890c9307
LittleCube <littlecubehax@gmail.com>
2024-12-02 20:04:52 -0500

    finally remove Clique options

--- worlds/mm_recomp/Options.py
+++ worlds/mm_recomp/Options.py
@@ -5,28 +5,6 @@ from typing import Dict
 from Options import Choice, Option, Toggle, StartInventoryPool, DeathLink, PerGameCommonOptions
 
 
-#class HardMode(Toggle):
-#    """Only for the most masochistically inclined... Requires button activation!"""
-#    display_name = "Hard Mode"
-
-
-#class ButtonColor(Choice):
-#    """Customize your button! Now available in 12 unique colors."""
-#    display_name = "Button Color"
-#    option_red = 0
-#    option_orange = 1
-#    option_yellow = 2
-#    option_green = 3
-#    option_cyan = 4
-#    option_blue = 5
-#    option_magenta = 6
-#    option_purple = 7
-#    option_pink = 8
-#    option_brown = 9
-#    option_white = 10
-#    option_black = 11
-
-
 class LogicDifficulty(Choice):
     """Set the logic difficulty used when generating."""
     display_name = "Logic Difficulty"
