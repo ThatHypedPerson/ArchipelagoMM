3373a0f2128914bc2a5c0cd2233998098224d519
LittleCube <littlecubehax@gmail.com>
2025-01-25 00:11:01 -0500

    Revert "add cow checks" (#15)

--- worlds/mm_recomp/Items.py
+++ worlds/mm_recomp/Items.py
@@ -450,7 +450,7 @@ item_data_table: Dict[str, MMRItemData] = {
     "Red Rupee": MMRItemData(
         code=0x3469420000004,
         type=ItemClassification.filler,
-        num_exist=37 # lol
+        num_exist=29
         # ~ num_exist=29
     ),
     "Purple Rupee": MMRItemData(
